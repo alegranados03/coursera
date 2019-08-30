679549873ac8585e6ec2f61cf2bc3fe6cb04d78b
alegranados03 <alegranados03@gmail.com>
2019-08-29 18:27:32 -0600

    final answer

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -2,17 +2,13 @@
 
 
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
-
-    return max_product
+    numbers.sort(reverse=True)
+    return numbers[0] * numbers[1]
 
 
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
+
+
