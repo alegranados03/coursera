72c2972312176fa220ff36b6a8561192d76a72e1
alegranados03 <alegranados03@gmail.com>
2019-09-10 20:57:51 -0600

    week 3 2 exercises

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,15 +1,26 @@
 # Uses python3
 import sys
 
-def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
 
+def get_optimal_value(capacity, weights, values):
+    a = value = 0
+    unit_vals = [x / y for x,y in zip(values,weights)]
+
+    for _ in range(len(weights)):
+        i = weights.index(min(weights))
+
+        if (capacity == 0): return value
+        a = min(weights[i],capacity)
+        capacity = capacity - a
+        value += a * unit_vals[i]
+
+        weights.pop(i)
+        unit_vals.pop(i)
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
+    data = list(map(int, input().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
